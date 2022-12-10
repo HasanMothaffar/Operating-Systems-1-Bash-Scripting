562af45bdc843a50e3b71f6d26ee00f22bf6d629
RedWn <redwaniscool@gmail.com>
2022-12-10 12:48:00 +0200

    finish second question

--- q2.sh
+++ q2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GLOBAL_DATABASE_FILENAME=""
+GLOBAL_DATABASE_FILENAME="" #maybe we should make a variable for the file name and another variable for the path
 SCRIPT_PROMPT="
 Q1 (first.os1db):
 1) Backup the database
@@ -13,7 +13,7 @@ backup_database() {
     read -p "Enter the backup name (with file extension .zip or .gz): " KEY
     if [[ "$KEY" =~ .*\.zip ]]; then
         echo "Using zip for backup (file has .zip extension)..."
-        sudo zip /home/os1/${KEY%".zip"} GLOBAL_DATABASE_FILENAME
+        sudo zip /home/os1/${KEY%".zip"} GLOBAL_DATABASE_FILENAME #maybe put $2 here?
         echo "Backup done (/home/os1/$KEY)"
     elif [[ "$KEY" =~ .*\.gz ]]; then
     echo "Using gz for backup (file has .gz extension)..."
@@ -22,11 +22,22 @@ backup_database() {
     fi
 }
 
-Restore_database() {
+restore_database() {
     read -p "Enter the absolute path of the backup file: " KEY
     if [[ "$KEY" =~ .*\.zip ]]; then
         unzip $KEY -d GLOBAL_DATABASE_FILENAME
     elif [[ "$KEY" =~ .*\.gz ]]; then
         tar -zxvf $KEY -C GLOBAL_DATABASE_FILENAME
+    fi
+}
+enable_automatic_backups(){
+    read -p "Please enter your backup schedule (daily, weekly or monthly): " KEY1
+    read -p "Max number of backup files to keep: " KEY2
+    if [[ "$KEY" == "daily" ]]; then
+        0 0 * * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2
+    if [[ "$KEY" == "weekly" ]]; then
+        0 0 * * 0 /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2
+    if [[ "$KEY" == "monthly" ]]; then
+        0 0 1 * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2
     fi
 }
