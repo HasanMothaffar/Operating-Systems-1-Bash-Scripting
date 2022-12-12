9b185758c8bffeb8b72d544bd2fd6af1e0b343e4
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-12 11:53:55 +0300

    Upload first attempt of question 5

--- q5-advanced-search.bash
+++ q5-advanced-search.bash
@@ -1,19 +1,39 @@
-find -name "*.txt" -o -name "*.js"
 
-function search_for_dirs_or_files_pseudocode() {
-    arguments_array=$@
+function search_for_dirs_or_files_without_multithreading() {
+    # Usage: ./my_script place-to-search-from arg1 arg2 arg3
+    # E.g. ./my_script / opt users_management.js another-file
     
-    directory_to_start_searching_from = arguments_array[0]
+    args=("$@")
+    ALL_ARGS_EXCEPT_FOR_FIRST_ONE=("${args[@]:1}")
 
-    arguments_array.remove(firstItem)
+    DIRECTORY_TO_START_SEARCHING_FORM="${args[0]}"
+    FIND_EXPRESSION="find $DIRECTORY_TO_START_SEARCHING_FORM "
 
-    find_expression = "";
+    # I hate bash: https://stackoverflow.com/questions/68881897/bash-get-last-index-of-array
+    LAST_INDEX=$((${#ALL_ARGS_EXCEPT_FOR_FIRST_ONE[@]} - 1))
 
-    foreach (arg in arguments_array) {
-        find_expression += "-name $arg"
+    for i in "${!ALL_ARGS_EXCEPT_FOR_FIRST_ONE[@]}"; do
+        FIND_EXPRESSION+="-name ${ALL_ARGS_EXCEPT_FOR_FIRST_ONE[$i]}"
+        if [ "$i" -ne "$LAST_INDEX" ] 
+            then
+            FIND_EXPRESSION+=" -o "
+        fi
+    done
 
-        if (find_expression is not last) find_expression += " -o "
-    }
+    SPECIAL_PRINT_FORMAT=" -printf %M\" \"%m\" User: \"%u/\" Group:\"%g/\" Name:\"%p\\\n"
+    REDIRECT_ERROR_TO_NULL=" 2>/dev/null"
+    
+    FIND_EXPRESSION+="$SPECIAL_PRINT_FORMAT"
+    FIND_EXPRESSION+="$REDIRECT_ERROR_TO_NULL"
 
-    find $directory_to_start_searching_from $find_expression
+    echo "Searching from: $DIRECTORY_TO_START_SEARCHING_FORM"
+    echo "Final expression: $FIND_EXPRESSION"
+    echo ""
+    eval "$FIND_EXPRESSION"
+
+    
+    # # https://unix.stackexchange.com/questions/353460/list-permissions-with-find-command
+    # cacheme find -name "opt" -printf "Permissions="%M\\t"User="%u" Group="%g\\t"Path="%p\\n 2>/dev/null
 }
+
+search_for_dirs_or_files_pseudocode "$@"
