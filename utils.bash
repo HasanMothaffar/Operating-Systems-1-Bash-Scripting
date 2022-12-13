6a27db21a3f6235c3fa43e46a1af90570fb7c1ca
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-13 16:36:51 +0300

    Rename variables in utils

--- utils.bash
+++ utils.bash
@@ -1,12 +1,12 @@
 function __register_cronjob() {
     CRONJOB="$1"
 
-    crontab -l > CURRENT_CRONJOBS_FILE
-    echo "$CRONJOB" >> CURRENT_CRONJOBS_FILE
+    crontab -l > current_cronjobs.txt
+    echo "$CRONJOB" >> current_cronjobs.txt
 
-    crontab CURRENT_CRONJOBS_FILE
+    crontab current_cronjobs.txt
     echo "Added cronjob: $CRONJOB successfully!"
 
     # rm because the current cron jobs get written to a local file in the invoking directory
-    rm CURRENT_CRONJOBS_FILE
+    rm current_cronjobs.txt
 }
