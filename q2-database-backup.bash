1f0cd289b2907ad9762656074224d58aba2c1b31
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-13 22:30:09 +0300

    Refactor q2
    Rename variables
    Extract main function and process arguments function
    Add references for arcane bash operations

--- q2-database-backup.bash
+++ q2-database-backup.bash
@@ -1,103 +1,136 @@
 #!/bin/bash
 
-#executing this will automatically activate SCRIPT_PROMPT cli and then it's pretty straight forward just as the homework says
+source utils.bash
 
-MODE=0                                 #--backup will set to 1 --restore will set 2
-OUTPUTDIR=""                           #--outputdir to set auto backup output path
-MAX=0                                  #--max to set max files to auto backup
+USAGE="
+./q2-database-backup.bash [--backup] | [--max MAX_NUMBER_OF_BACKUPS]
+"
 
+if [ $# = 0 ] 
+    then
+    echo "$USAGE"
+    exit
+fi
 
-function backup_database() {
-    if [[ MAX -gt 0 ]]; then
-        temp=$(cat /home/os1/backup.log)
-        temp2=${temp#*-}
-        sudo zip "$OUTPUTDIR/$(($temp2 % $MAX))" "$GLOBAL_DATABASE_FILENAME"
-        echo "$MAX-"$(($temp2 + 1))"" | tee /home/os1/backup.log
-    else
-        read -p "Enter the backup name (with file extension .zip or .tar.gz): " KEY
-        if [[ "$KEY" =~ .*\.zip ]]; then
-            echo "Using zip for backup (file has .zip extension)..."
-            ip /home/os1/"${KEY%".zip"}" "$GLOBAL_DATABASE_FILENAME"
-            echo "Backup done (/home/os1/$KEY)"
-        elif [[ "$KEY" =~ .*\.tar.gz ]]; then
-            echo "Using gz for backup (file has .gz extension)..."
-            tar -czf "/home/os1/$KEY" "$GLOBAL_DATABASE_FILENAME"
-            echo "Backup done (/home/os1/$KEY)"
-        fi
+# ARGS
+AUTOMATIC_BACKUP=false
+BACKUP_DIR=""
+MAX_NUMBER_OF_BACKUPS=0
+
+# VARS LOCAL TO THIS SCRIPT
+DATABASE_FILENAME=""
+
+function manually_backup_database() {
+    read -r -p "Enter the backup name (with file extension .zip or .tar.gz): " BACKUP_FILENAME
+
+    if [[ "$BACKUP_FILENAME" =~ .*\.zip ]]; then
+        echo "Using zip for backup (file has .zip extension)..."
+        zip "$BACKUP_FILENAME" "$DATABASE_FILENAME"
+
+    elif [[ "$BACKUP_FILENAME" =~ .*\.tar.gz ]]; then
+        echo "Using gz for backup (file has .gz extension)..."
+        tar -czf "$BACKUP_FILENAME" "$DATABASE_FILENAME"
     fi
+
+    echo "Backup done ($BACKUP_FILENAME)"
 }
 
-function restore_database() {
-    read -r -p "Enter the absolute path of the backup file: " PATH
+function automatically_backup_database() {
+    BACKUPS_COUNT=$(ls /opt/backups | wc -l)
+    
+    if [ "$BACKUPS_COUNT" -ge "$MAX_NUMBER_OF_BACKUPS" ]
+        then
+        # Remove file with oldest modified date
+        rm /opt/backups/"$(ls /opt/backups -t | tail -1)"
+    fi
 
-    if [[ "$PATH" =~ .*\.zip ]]; then
-        unzip "$PATH"
-    elif [[ "$PATH" =~ .*\.tar\.gz ]]; then
-        tar -zxf "$PATH"
+    # https://stackoverflow.com/questions/1401482/yyyy-mm-dd-format-date-in-shell-script
+    DATE=$(date '+%Y-%m-%d_%H:%M:%S')
+    sudo zip "$BACKUP_DIR/${DATE}_${DATABASE_FILENAME}.zip" "$DATABASE_FILENAME"
+}
+
+function restore_database_from_path() {
+    read -r -p "Enter the backup name (with file extension .zip or .tar.gz): " FILEPATH
+    
+    # The target directory after uncompressing the file will be the current working directory (i.e. '.')
+    # Therefore, the `unzip` or `tar` commands will prompt you if you want to replace the db file after uncompressing
+    if [[ "$FILEPATH" =~ .*\.zip ]]; then
+        unzip "$FILEPATH"
+    elif [[ "$FILEPATH" =~ .*\.tar\.gz ]]; then
+        tar -zxf "$FILEPATH"
     fi
 }
 
 function enable_automatic_backups() {
-    >/home/os1/backup.log
-    read -p "Please enter your backup schedule (daily, weekly or monthly): " KEY1
-    read -p "Max number of backup files to keep: " KEY2
-    echo "$KEY2-0" | tee /home/os1/backup.log
-    crontab -l >POPCRON
-    if [[ "$KEY1" == "daily" ]]; then
-        echo "0 0 * * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2" >>POPCRON
-        echo "Adding(0 0 * * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2)"
-    elif [[ "$KEY1" == "weekly" ]]; then
-        echo "0 0 * * 0 /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2" >>POPCRON
-        echo "Adding(0 0 * * 0 /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2)"
-    elif [[ "$KEY1" == "monthly" ]]; then
-        echo "0 0 1 * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2" >>POPCRON
-        echo "Adding(0 0 1 * * /home/os1/q2.sh /home/os1/first.os1db --backup --outputdir /opt/backups --max $KEY2)"
+    read -r -p "Please enter your backup schedule (daily, weekly or monthly): " BACKUP_SCHEDULE
+    read -r -p "Max number of backup files to keep: " MAX_NUMBER_OF_BACKUPS
+
+    CRONJOB_SCHEDULE=""
+
+    if [[ "$BACKUP_SCHEDULE" == "daily" ]]; then CRONJOB_SCHEDULE="0 0 * * *"
+    elif [[ "$BACKUP_SCHEDULE" == "weekly" ]]; then CRONJOB_SCHEDULE="0 0 * * 0"
+    elif [[ "$BACKUP_SCHEDULE" == "monthly" ]]; then CRONJOB_SCHEDULE="0 0 1 * *"
     fi
-    echo "done"
+
+    SCRIPT_PATH=$(__get_script_fullpath)
+
+    # Sorry for hardcoding :(
+    HARDCODED_DB_PATH="/mnt/d/my-projects/operating-systems-y4t1/$DATABASE_FILENAME"
+    CRONJOB="$CRONJOB_SCHEDULE $SCRIPT_PATH $HARDCODED_DB_PATH --backup /opt/backups --max $MAX_NUMBER_OF_BACKUPS"
+    
+    __register_cronjob "$CRONJOB"
 }
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-    *db)
-        GLOBAL_DATABASE_FILENAME=$1
-        shift
-        ;;
-    --backup)
-        MODE=1
-        shift
-        ;;
-    --restore)
-        MODE=2
-        shift
-        ;;
-    --outputdir)
-        OUTPUTDIR="/opt/backups"
-        shift 2
-        ;;
-    --max)
-        MAX=$2
-        shift 2
-        ;;
-    esac
-done
-
-if [[ MAX -eq 0 ]]; then
-    while true; do
-        read -r -p "$SCRIPT_PROMPT" PREF
-        case $PREF in
-        1) backup_database ;;
-        2) restore_database ;;
-        3) enable_automatic_backups ;;
-        4)
-            echo "Exiting program."
-            exit 0
+function parse_commandline_arguments() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+        *db)
+            DATABASE_FILENAME=$1
+            shift
             ;;
-        *) echo "Unknown key." ;;
+        --backup)
+            AUTOMATIC_BACKUP=true
+            BACKUP_DIR="$2"
+            shift 2
+            ;;
+        --max)
+            MAX_NUMBER_OF_BACKUPS=$2
+            shift 2 # Because we will read '--max' and the argument directly after it, which is the actual number ($2)
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit
+            ;;
         esac
     done
-else
-    case $MODE in
-    1) backup_database ;;
-    2) restore_database ;;
-    esac
-fi
+}
+
+function main() {
+    SCRIPT_PROMPT="
+Q2 backup / restore ($DATABASE_FILENAME):
+1) Backup the database
+2) Restore a database
+3) Enable automatic backups
+4) Exit
+Please select one of the options above: "
+
+    if [ $AUTOMATIC_BACKUP = true ]; then automatically_backup_database
+    else
+        while true; do
+            read -r -p "$SCRIPT_PROMPT" OPTION
+            case $OPTION in
+            1) manually_backup_database ;;
+            2) restore_database_from_path ;;
+            3) enable_automatic_backups ;;
+            4)
+                echo "Exiting program."
+                exit 0
+                ;;
+            *) echo "Unknown key." ;;
+            esac
+        done
+    fi
+}
+
+parse_commandline_arguments "$@"
+main "$@"
