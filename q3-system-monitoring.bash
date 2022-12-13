6341f2b214fd2a9ec181ef213dd32d62525629ef
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-13 10:49:08 +0300

    Add cronjob registration logic to q3

--- q3-system-monitoring.bash
+++ q3-system-monitoring.bash
@@ -1,24 +1,60 @@
 #!/bin/bash
 
-# should add "0 0 * * * /home/os1/q3-system-monitoring.bash" to crontab
+MONITORING_RESULTS_FILE="/opt/monitoring.txt"
 
-DATE=$(date | grep -o [A-Z][a-z][a-z].[0-9][0-9])
-echo $DATE >>/opt/monitoring.txt
+function write_current_date_monitoring_file() {
+    DATE=$(date | grep -o "[A-Z][a-z][a-z].[0-9][0-9]")
+    echo "$DATE" >> "$MONITORING_RESULTS_FILE"
+}
 
-for ((i = 0; i < 720; i++)); do
-    #there's 720 5sec in an hour cron will execute the command and sleep with for-loop will rexecute
-    echo "RAM, Disk, CPU" >>/opt/monitoring.txt
-    DSK=$(df . | grep -o [^s].%)
-    RAM=$(free | grep Mem | awk '{print $3/$2 * 100}')
-    RAM=$(printf "%.0f" "$RAM")
-    RAM="$RAM%"
-    CPU=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) ; }' \
-        <(grep 'cpu ' /proc/stat) <(
-            sleep 1
-            grep 'cpu ' /proc/stat
-        ))
-    CPU=$(printf "%.0f" "$CPU")
-    CPU="$CPU%"
-    echo "$RAM, $DSK, $CPU" >>/opt/monitoring.txt
-    sleep 4
-done
+function main() {
+    write_current_date_monitoring_file
+
+    ONE_HOUR_IN_SECONDS=3600
+    SCRIPT_RUNNING_INTERVAL_IN_SECONDS=5
+    ITERATIONS_COUNT=$((ONE_HOUR_IN_SECONDS / SCRIPT_RUNNING_INTERVAL_IN_SECONDS))
+
+    for ((i = 0; i < ITERATIONS_COUNT; i++)); do
+        #there's 720 5sec in an hour cron will execute the command and sleep with for-loop will rexecute
+        
+        DISK_USAGE=$(df . | grep -o "[^s].%")
+
+        RAM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100}')
+        RAM_USAGE=$(printf "%.0f" "$RAM_USAGE")
+        RAM_USAGE="$RAM_USAGE%"
+
+        CPU_USAGE=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) ; }' \
+            <(grep 'cpu ' /proc/stat) <(
+                sleep 1
+                grep 'cpu ' /proc/stat
+            ))
+        CPU_USAGE=$(printf "%.0f" "$CPU_USAGE")
+        CPU_USAGE="$CPU_USAGE%"
+
+        echo "RAM, DISK, CPU" >> "$MONITORING_RESULTS_FILE"
+        echo "$RAM_USAGE, $DISK_USAGE, $CPU_USAGE" >> "$MONITORING_RESULTS_FILE"
+
+        sleep $SCRIPT_RUNNING_INTERVAL_IN_SECONDS
+    done
+}
+
+function register_cronjob() {
+    # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+    BASENAME=$( basename -- "$0" )
+    WORKING_DIRECTORY=$( pwd )
+    FULL_PATH="${WORKING_DIRECTORY}/${BASENAME}"
+
+    crontab -l > CURRENT_CRONJOBS_FILE
+
+    Q3_CRONJOB="0 0 * * * $FULL_PATH"
+    echo "$Q3_CRONJOB" >> CURRENT_CRONJOBS_FILE
+
+    crontab CURRENT_CRONJOBS_FILE
+
+    # rm because the current cron jobs get written to a local file in the invoking directory
+    rm CURRENT_CRONJOBS_FILE
+
+}
+
+# register_cronjob "$@"
+main
